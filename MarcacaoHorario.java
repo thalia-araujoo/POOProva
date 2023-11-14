e4dce75501c76b4766fd2acb919964c350b10773
Liah Souza <115299056+thalia-araujoo@users.noreply.github.com>
2023-11-14 15:15:44 -0300

    Add files via upload

---
+++ MarcacaoHorario.java
@@ -0,0 +1,20 @@
+class MarcacaoHorario{
+    private String nome = nome;
+    private String horario = horario;
+
+  
+    public void MarcacaoHorario( string nome, String horario){
+        this.nome = nome;
+        this.horario = horario;
+
+    }
+
+    public void Retornar_horario(String horario){
+        return horario;
+    }
+
+    public void Retornar_NomePac(String nome){
+        return nome;
+    }
+
+}
